fde4de34b4ee2911f38c6ea26ab40ebe17a243c7
sae1013 <sae1013@gmail.com>
2022-01-31 21:23:51 +0900

    드래그기능 구현

--- src/App.js
+++ src/App.js
@@ -4,39 +4,15 @@ import Draggable from "./Draggable";
 import classes from "./App.module.css";
 
 function App() {
-  const [dotted,setDotted] = React.useState(false)
-  const dropHandler = (e) => {
-    e.preventDefault()
-    console.log(e.dataTransfer.getData('key'))
-    setDotted(false)
-    // console.log('onDrop')
-  }
-  const dragOverHandler =(e) => {
-    e.preventDefault()
-    e.dataTransfer.dropEffect = "copy"
-    // console.log('onDragover')
-  }
-  const dragEnterHandler = (e) => {
-    console.log('dragEnter')
-    setDotted(true)
-  }
-  const dragLeaveHandler = (e) => {
-    console.log('dragLeave')
-    setDotted(false)
-  }
-
+  
   return (
-    <div className="App">
-      <Draggable />
-      <div className={`${classes.dropzone} ${dotted ? classes.dotted: ''}`} 
-        droppable = "true"
-        onDrop = {dropHandler}
-        onDragOver = {dragOverHandler}
-        onDragEnter = {dragEnterHandler}
-        onDragLeave = {dragLeaveHandler}
-        >
-        DropZone
-      </div>
+    <div className={classes.App}>
+      <Draggable>
+        {/* <div className={classes.box}>box</div> */}
+      </Draggable>
+      
+      
+      
     </div>
   );
 }
--- src/Draggable.js
+++ src/Draggable.js
@@ -1,28 +1,41 @@
 import React from "react";
 import classes from "./Draggable.module.css";
 
-function Draggable() {
+function Draggable({ children }) {
+  const targetRef = React.useRef(null)
+  const mouseDown = (e) => {
+    const [startX,startY] = [e.clientX,e.clientY]
+    const mouseMove = (e) => {
+      // Do something in Move (a.k.a drag we think)
+      
+      // targetRef.current.style.left = `${e.clientX - e.target.clientWidth/2}px`
+      
+      if (e.target.id === "target") {
+        console.log(e.clientX-startX)
+        targetRef.current.style.opacity= "0.7"
+        targetRef.current.style.left = `${e.clientX-startX}px`
+        targetRef.current.style.top = `${e.clientY-startY}px`
+      }
+    };
 
-  const dragHandler = (e) => { 
-    console.log('dragHandler!')
-  }
+    const mouseUp = (e) => {
+      window.removeEventListener("mousemove", mouseMove);
+      window.removeEventListener("mouseup", mouseUp);
+      targetRef.current.style.opacity= "1"
+      
+    };
 
-  const dragStartHandler = (e) => {
-    e.dataTransfer.setData('key','드래그객체')
-    e.target.style.opacity ="0.5"
-    // e.dataTransfer.effectAllowed = "moveCopy";
-  }
-  const dragEndHandler = (e) => {
-    e.target.style.opacity = "1"
-  }
+    window.addEventListener("mousemove", mouseMove);
+    window.addEventListener("mouseup", mouseUp);
+  };
+
   return (
-    <div className={classes.item} 
-      onDrag = {dragHandler}
-      onDragStart = {dragStartHandler} 
-      onDragEnd = {dragEndHandler}
-      draggable>
-      DragItem
-    </div>
+    <div
+      id="target"
+      ref = {targetRef}
+      className={classes.container}
+      onMouseDown={mouseDown}
+    ></div>
   );
 }
 
