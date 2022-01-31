6f228e0f195cf6f8ce91a2bdc9828a8a5471450a
sae1013 <sae1013@gmail.com>
2022-01-31 21:56:23 +0900

    마우스이벤트 개선

--- src/Draggable.js
+++ src/Draggable.js
@@ -3,18 +3,20 @@ import classes from "./Draggable.module.css";
 
 function Draggable({ children }) {
   const targetRef = React.useRef(null)
+  React.useEffect(()=>{
+    console.dir(targetRef.current)
+  })
   const mouseDown = (e) => {
+    const [curLeft,curTop] = [targetRef.current.offsetLeft,targetRef.current.offsetTop]
     const [startX,startY] = [e.clientX,e.clientY]
     const mouseMove = (e) => {
       // Do something in Move (a.k.a drag we think)
       
-      // targetRef.current.style.left = `${e.clientX - e.target.clientWidth/2}px`
-      
       if (e.target.id === "target") {
-        console.log(e.clientX-startX)
+        
         targetRef.current.style.opacity= "0.7"
-        targetRef.current.style.left = `${e.clientX-startX}px`
-        targetRef.current.style.top = `${e.clientY-startY}px`
+        targetRef.current.style.left = `${curLeft+e.clientX-startX}px`
+        targetRef.current.style.top = `${curTop+e.clientY-startY}px`
       }
     };
 
