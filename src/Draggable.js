d56f934ad154bf5c3858f3f5f05d6620c1d8838e
sae1013 <sae1013@gmail.com>
2022-01-31 21:57:01 +0900

    마우스이벤트 개선버전

--- src/Draggable.js
+++ src/Draggable.js
@@ -9,15 +9,12 @@ function Draggable({ children }) {
   const mouseDown = (e) => {
     const [curLeft,curTop] = [targetRef.current.offsetLeft,targetRef.current.offsetTop]
     const [startX,startY] = [e.clientX,e.clientY]
-    const mouseMove = (e) => {
-      // Do something in Move (a.k.a drag we think)
-      
-      if (e.target.id === "target") {
-        
+    
+    const mouseMove = (e) => {  
         targetRef.current.style.opacity= "0.7"
         targetRef.current.style.left = `${curLeft+e.clientX-startX}px`
         targetRef.current.style.top = `${curTop+e.clientY-startY}px`
-      }
+      
     };
 
     const mouseUp = (e) => {
