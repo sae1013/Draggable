8cef70045e6440c0906cf5b12ef3ed35eb7ef7e5
sae1013 <sae1013@gmail.com>
2022-02-01 16:27:30 +0900

    REFACTOR: 코드정리

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from "react";
 import logo from "./logo.svg";
 import Draggable from "./Draggable";
-// import Draggable from 'react-draggable';
 import classes from "./App.module.css";
 
-function Parent({children}) {
-  const ReactElement = React.cloneElement(children,{style:{backgroundColor:'blue'},onClick:()=>{alert(123)}})
-  return <React.Fragment>{ReactElement}</React.Fragment>;
-}
-function Child() {
-  return <div onClick={()=>{console.log('자식자식')}}>자식</div>;
-}
-function Child1(){
-  return <div style={{width:'100px',height:'100px',backgroundColor:'red'}}>Child1</div>
-} 
 function App() {
   return (
     <div className={classes.App}>
-      {/* <Parent>
-        <Child></Child>
-      </Parent> */}
       <Draggable>
         <div className={classes.box}>박스</div>
       </Draggable>
--- src/Draggable.js
+++ src/Draggable.js
@@ -1,60 +1,57 @@
-import React,{forwardRef} from "react";
-import classes from "./Draggable.module.css";
+import React from "react";
 
 function Draggable({ children }) {
-  const targetRef = React.useRef(null)
-  
-  // React.useEffect(()=>{
-  //   console.log(targetRef.current)
-  // })
-  
+  const targetRef = React.useRef(null);
+
   const mouseDown = (e) => {
-    const [curLeft,curTop] = [targetRef.current.offsetLeft,targetRef.current.offsetTop]
-    const [startX,startY] = [e.clientX,e.clientY]
-    const [clientWidth,clientHeight] = [targetRef.current.clientWidth,targetRef.current.clientHeight]
-    const viewportWidth = window.innerWidth
-    const viewportHeight = window.innerHeight
-    
-    const mouseMove = (e) => {  
-        targetRef.current.style.opacity= "0.7"
-        let nextOffsetX = curLeft + e.clientX-startX 
-        let nextOffsetY = curTop + e.clientY-startY 
-        
-        if(nextOffsetX < 0){
-          nextOffsetX = 0
-        }
-        else if(nextOffsetX+clientWidth > viewportWidth){
-          nextOffsetX = viewportWidth-clientWidth
-        }
-        if(nextOffsetY < 0) {
-          nextOffsetY = 0
-        }
-        else if(nextOffsetY + clientHeight > viewportHeight) {
-          nextOffsetY = viewportHeight-clientHeight
-        }
-        targetRef.current.style.left = `${nextOffsetX}px`
-        targetRef.current.style.top = `${nextOffsetY}px`
-      
+    const [curLeft, curTop] = [
+      targetRef.current.offsetLeft,
+      targetRef.current.offsetTop,
+    ];
+    const [startX, startY] = [e.clientX, e.clientY];
+    const [clientWidth, clientHeight] = [
+      targetRef.current.clientWidth,
+      targetRef.current.clientHeight,
+    ];
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+
+    const mouseMove = (e) => {
+      targetRef.current.style.opacity = "0.7";
+      let nextOffsetX = curLeft + e.clientX - startX;
+      let nextOffsetY = curTop + e.clientY - startY;
+
+      if (nextOffsetX < 0) {
+        nextOffsetX = 0;
+      } else if (nextOffsetX + clientWidth > viewportWidth) {
+        nextOffsetX = viewportWidth - clientWidth;
+      }
+      if (nextOffsetY < 0) {
+        nextOffsetY = 0;
+      } else if (nextOffsetY + clientHeight > viewportHeight) {
+        nextOffsetY = viewportHeight - clientHeight;
+      }
+      targetRef.current.style.left = `${nextOffsetX}px`;
+      targetRef.current.style.top = `${nextOffsetY}px`;
     };
 
     const mouseUp = (e) => {
       window.removeEventListener("mousemove", mouseMove);
       window.removeEventListener("mouseup", mouseUp);
-      targetRef.current.style.opacity= "1"
-      
+      targetRef.current.style.opacity = "1";
     };
 
     window.addEventListener("mousemove", mouseMove);
     window.addEventListener("mouseup", mouseUp);
   };
-  
-  const TargetComponent = React.cloneElement(children,{ref:targetRef,onMouseDown:mouseDown,style:{position:'absolute'}})
 
-  return (
-    <React.Fragment>
-      {TargetComponent}
-    </React.Fragment>
-  )
+  const TargetComponent = React.cloneElement(children, {
+    ref: targetRef,
+    onMouseDown: mouseDown,
+    style: { position: "absolute" },
+  });
+
+  return <React.Fragment>{TargetComponent}</React.Fragment>;
 }
 
 export default Draggable;
